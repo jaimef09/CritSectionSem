915fffb720ebd35c1350f1a47b0ab21799ffdfff
jaimef09 <jaimef09@ucm.es>
2022-02-20 20:18:41 +0100

    Lock

---
+++ 04_decker_lock.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from multiprocessing import Process, Lock
+from multiprocessing import Value
+
+N = 8
+
+def task(common, tid, l):
+    a = 0
+    for i in range(100):
+        print(f'{tid}-{i}: Non-critical Section')
+        a += 1
+        print(f'{tid}-{i}: End of non-critical Section')
+
+        l.acquire()
+        try:
+            print(f'{tid}-{i}: Critical section')
+            v = common.value + 1
+            print(f'{tid}-{i}: Inside critical section')
+            common.value = v
+            print(f'{tid}-{i}: End of critical section')
+        finally:
+            l.release()
+
+def main():
+    lp = []
+    common = Value('i', 0)
+    lock = Lock()
+    for tid in range(N):
+        lp.append(Process(target=task, args=(common, tid, lock)))
+    print(f"Valor inicial del contador {common.value}")
+    for p in lp:
+        p.start()
+    
+    for p in lp:
+        p.join()
+        
+    print(f"Valor final del contador {common.value}")
+    print("fin")
+    
+if __name__ == "__main__":
+    main()
+
