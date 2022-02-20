9907fd69a685ac460e67890d2e6efdd279b1a92b
jaimef09 <jaimef09@ucm.es>
2022-02-20 20:19:11 +0100

    BoundedSemaphore

---
+++ 04_decker_BoundedSemaphore.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from multiprocessing import Process, BoundedSemaphore
+from multiprocessing import Value
+
+N = 8
+
+def task(common, tid, semaphore):
+    a = 0
+    for i in range(100):
+        print(f'{tid}-{i}: Non-critical Section')
+        a += 1
+        print(f'{tid}-{i}: End of non-critical Section')
+
+        semaphore.acquire()
+        try:
+            print(f'{tid}-{i}: Critical section')
+            v = common.value + 1
+            print(f'{tid}-{i}: Inside critical section')
+            common.value = v
+            print(f'{tid}-{i}: End of critical section')
+        finally:
+            semaphore.release()
+
+def main():
+    lp = []
+    common = Value('i', 0)
+    semaphore = BoundedSemaphore(1)
+    for tid in range(N):
+        lp.append(Process(target=task, args=(common, tid, semaphore)))
+    print(f"Valor inicial del contador {common.value}")
+    for p in lp:
+        p.start()
+    
+    for p in lp:
+        p.join()
+        
+    print(f"Valor final del contador {common.value}")
+    print("fin")
+    
+if __name__ == "__main__":
+    main()
